d20a034e4e3d8e7bb2a561e9f1460451f7e406ef
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-06-01 10:53:22 -0500

    Add loading state management in NewItem widget; disable buttons during item submission and show progress indicator.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,10 +19,14 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _seveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
       final url = Uri.https(
         'shopping-list-cecb0-default-rtdb.firebaseio.com',
         'shopping-list.json',
@@ -140,14 +144,22 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
-                    onPressed: _seveItem,
-                    child: const Text('Add item'),
+                    onPressed: _isSending ? null : _seveItem,
+                    child: _isSending
+                        ? const SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : const Text('Add item'),
                   ),
                 ],
               ),
